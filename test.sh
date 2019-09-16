588a3c9512e98ee2dfa4c07b5b6fa215a399b729
Shadi Akiki <shadiakiki1986@gmail.com>
2019-09-16 14:04:07 +0300

    add optimize tests

--- test.sh
+++ test.sh
@@ -12,6 +12,12 @@ AWS_PROFILE=autofitcloud AWS_DEFAULT_REGION=eu-central-1 isitfit
 echo "Test 3: default profile in region with 0 ec2 instances"
 AWS_DEFAULT_REGION=eu-central-1 isitfit
 
+echo "Test 4: optimize with default profile"
+isitfit --optimize
+
+echo "Test 5: optimize in region with 0 ec2 instances"
+AWS_DEFAULT_REGION=eu-central-1 isitfit --optimize
+
 # done
 # `set -x` doesn't let the script reach this point in case of any error
 echo "Tests completed"
