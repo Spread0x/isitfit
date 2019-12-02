5c350af1e5cd705b6ed3e62807370b2bcdca53b5
Shadi Akiki <shadiakiki1986@gmail.com>
2019-12-02 13:43:56 +0200

    enh: same for `optimize` and `ec2` in `ec2_optimize.py`

--- isitfit/cost/__init__.py
+++ isitfit/cost/__init__.py
@@ -1,5 +1,5 @@
 from isitfit.cost.ec2_analyze import pipeline_factory as ec2_cost_analyze
-from isitfit.cost.ec2.pipeline_factory import ec2_cost_optimize
+from isitfit.cost.ec2_optimize import pipeline_factory as ec2_cost_optimize
 from isitfit.cost.redshift.pipeline_factory import redshift_cost_analyze, redshift_cost_optimize
 from isitfit.cost.account_cost_analyze  import pipeline_factory as account_cost_analyze
 from isitfit.cost.account_cost_optimize import pipeline_factory as account_cost_optimize
